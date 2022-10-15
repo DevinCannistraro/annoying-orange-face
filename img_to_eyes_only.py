e0547a8423587a760047b5c83ad3d1d48827819c
DevinCannistraro <38543372+DevinCannistraro@users.noreply.github.com>
2022-10-15 15:55:30 -0400

    m

--- img_to_eyes_only.py
+++ img_to_eyes_only.py
@@ -35,19 +35,31 @@ def createBox(img,points):
 
     return imgCrop
 
-def anti_alias_resize_path(im,scaler,name):
+def resize_image_CV2(im,scalar):
+    width = int(im.shape[1] * scalar)
+    height = int(im.shape[0] * scalar)
+    dim = (width, height)
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    return resized
+
+def resize_points(points,scalar):
+    new_points = points * scalar
+    return new_points
+
+def anti_alias_resize_path_PIL(im,scaler,name):
     width, height = im.size
     im = im.resize((width * scaler, height * scaler), resample=Image.Resampling.LANCZOS)
     return im #im.save("TEMP_IMS//" + name + ".png")
-
 
 def do_PIL_processing(mouth_path,left_eye_path,right_eye_path):
     mouth_im = Image.open(mouth_path)
     left_eye_im = Image.open(left_eye_path)
     right_eye_im = Image.open(right_eye_path)
-    mouth_im = anti_alias_resize_path(mouth_im,2,"mouth_resize")
-    left_eye_im = anti_alias_resize_path(left_eye_im, 2, "left_resize")
-    right_eye_im = anti_alias_resize_path(right_eye_im, 2, "right_resize")
+    mouth_im = anti_alias_resize_path_PIL(mouth_im,2,"mouth_resize")
+    left_eye_im = anti_alias_resize_path_PIL(left_eye_im, 2, "left_resize")
+    right_eye_im = anti_alias_resize_path_PIL(right_eye_im, 2, "right_resize")
 
     blank_image = Image.new("RGBA",(1920,1080),(0,0,0,0))
 
@@ -105,8 +117,6 @@ def get_interpolated_points(np_points,desired_num_points):
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-cap = cv2.VideoCapture(5)
-
 if not os.path.exists("TEMP_IMS"):
     os.mkdir("TEMP_IMS")
 
@@ -133,12 +143,23 @@ if len(faces) > 0:
         #cv2.putText(compositImg,str(n),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255))
     if len(myPoints) > 0:
         myPoints = np.array(myPoints)
+        INITIAL_SCALE_FACTOR = 10 # must be int used to scale up to get smoother edges after slicing
+        DOWNSAMPLE_SCALE_FACTOR = .2 # scaled down after to increase edge smoothness
+        # scale up points so we can downscale after and have higher res
+        myPoints = resize_points(myPoints,INITIAL_SCALE_FACTOR)
+        img = resize_image_CV2(img,INITIAL_SCALE_FACTOR)
+
         mouth_points = np.int32([get_interpolated_points(myPoints[48:60],200)])
         imgMouth = createBox(img,mouth_points)
         left_eye_points = np.int32([get_interpolated_points(myPoints[36:42], 200)])
         imgLeftEye = createBox(img, left_eye_points)
         right_eye_points = np.int32([get_interpolated_points(myPoints[42:48], 200)])
         imgRightEye = createBox(img, right_eye_points)
+
+        #downscale
+        #imgMouth = resize_image_CV2(imgMouth, DOWNSAMPLE_SCALE_FACTOR)
+        #imgLeftEye = resize_image_CV2(imgLeftEye, DOWNSAMPLE_SCALE_FACTOR)
+        #imgRightEye = resize_image_CV2(imgRightEye, DOWNSAMPLE_SCALE_FACTOR)
 
         cv2_intermediate_mouth_path = "TEMP_IMS//intermediate_mouth.png"
         cv2_intermediate_left_eye_path = "TEMP_IMS//intermediate_left_eye.png"
